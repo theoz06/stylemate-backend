f38da1c3b1aaf01c9e85d0bd754a99a02d9959e3
Teofilus <t.ziraluo@yahoo.com>
2025-03-19 03:39:30 +0700

    initial commit: upload StyleMate Project

---
+++ stylemate/src/main/java/com/stylemate/app/StyleMateApplication.java
@@ -0,0 +1,13 @@
+package com.stylemate.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StyleMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StyleMateApplication.class, args);
+	}
+
+}
---
+++ stylemate/src/test/java/com/stylemate/app/StyleMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stylemate.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StyleMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
