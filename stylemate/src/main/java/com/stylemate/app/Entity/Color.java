1ac89520410aa699407399f6c2d7497500fe8065
Teofilus <t.ziraluo@yahoo.com>
2025-03-22 18:22:29 +0700

    Add ColorCategory entity and establish relationship with Color entity

--- stylemate/src/main/java/com/stylemate/app/Entity/Color.java
+++ stylemate/src/main/java/com/stylemate/app/Entity/Color.java
@@ -4,10 +4,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 
 @Data
 @NoArgsConstructor
@@ -20,4 +24,8 @@ public class Color {
     private Integer id;
 
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "color_category_id")
+    private ColorCategory colorCategory;
 }
---
+++ stylemate/src/main/java/com/stylemate/app/Entity/ColorCategory.java
@@ -0,0 +1,35 @@
+package com.stylemate.app.Entity;
+
+import javax.persistence.Entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+import javax.persistence.OneToMany;
+import javax.persistence.CascadeType;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "color_categories")
+public class ColorCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String description;
+
+    @OneToMany(mappedBy = "colorCategory", cascade = CascadeType.ALL , orphanRemoval = true)
+    private List<Color> colors = new ArrayList<>();
+    
+}
